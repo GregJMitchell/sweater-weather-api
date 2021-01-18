1f308fe1f40bde51a2abf3cb5baa193f575ef082
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-18 14:10:49 -0700

    Create users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user_params = JSON.parse(request.body.string, symbolize_names: true)
+
+    user = User.create(email: user_params[:email],
+                       password: user_params[:password],
+                       password_confirmation: user_params[:password_confirmation])
+
+    if user.save
+      render json: UserSerializer.new(user)
+    elsif User.find_by(email: user_params[:email])
+      render json: { message: 'unsuccessful', error: 'User already exists' },
+             status: :conflict
+    else
+      render json: { message: 'unsuccessful', error: 'Could not create user' },
+             status: :unprocessable_entity
+    end
+  end
+end
