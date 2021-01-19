c0ff8dbc75145e8fa42e28c6fe2e4c8580eea9fc
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-19 10:46:44 -0700

    Add conditional to check for no food searched

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -1,6 +1,10 @@
 class MunchiesFacade
   def self.find_munchies(cords, food, road_trip)
-    json = MunchiesService.find_munchies(cords[:lat], cords[:lng], food)
-    Munchie.new(json, road_trip)
+    if food != ''
+      json = MunchiesService.find_munchies(cords[:lat], cords[:lng], food)
+      Munchie.new(json, road_trip)
+    else
+      nil
+    end
   end
 end
