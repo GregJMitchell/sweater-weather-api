c086202551a65152e6fbb11ef6a02b1a9a30a0b6
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-19 10:34:30 -0700

    Add some sad path to munchies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -3,7 +3,12 @@ class Api::V1::MunchiesController < ApplicationController
     mapquest_params = {origin: params[:start], destination: params[:end]}
     cords = MapQuestFacade.get_cords(params[:end])
     road_trip = MapQuestFacade.road_trip(mapquest_params)
-    munchies = MunchiesFacade.find_munchies(cords, params[:food], road_trip)
-    require 'pry'; binding.pry
+    munchie = MunchiesFacade.find_munchies(cords, params[:food], road_trip)
+    if munchie.instance_of?(Munchie)
+      render json: MunchieSerializer.new(munchie)
+    else
+      render json: { message: 'unsuccessful', error: 'Something went wrong' },
+             status: :bad_request
+    end
   end
 end
