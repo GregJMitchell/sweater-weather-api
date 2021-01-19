d8fdfd67bb9b4e6db9bffec3b66d9cc133373b84
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-18 20:41:56 -0700

    Create road_trip weather poro

---
+++ app/poros/road_trip_weather.rb
@@ -0,0 +1,25 @@
+class RoadTripWeather
+  attr_reader :temperature,
+              :conditions
+
+  def initialize(weather, time)
+    @time = parse_time(time)
+    @temperature = get_temp(weather)
+    @conditions = weather[:hourly][@time][:weather].first[:description]
+  end
+
+  private
+
+  def parse_time(time)
+    (time / 3600.0).round(0)
+  end
+
+  def get_temp(weather)
+    temp = weather[:hourly][@time][:temp]
+    convert_temp(temp)
+  end
+
+  def convert_temp(temp)
+    (temp / 0.55555556 - 459.67).round(2)
+  end
+end
