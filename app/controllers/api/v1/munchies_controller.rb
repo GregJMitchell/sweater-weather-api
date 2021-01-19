66eebe968df3f8a1394dcb5c000f47c96dce02f9
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-19 10:47:00 -0700

    Add conditionals to check for sad path

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,13 +1,19 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
-    mapquest_params = {origin: params[:start], destination: params[:end]}
+    mapquest_params = { origin: params[:start], destination: params[:end] }
     cords = MapQuestFacade.get_cords(params[:end])
     road_trip = MapQuestFacade.road_trip(mapquest_params)
-    munchie = MunchiesFacade.find_munchies(cords, params[:food], road_trip)
-    if munchie.instance_of?(Munchie)
-      render json: MunchieSerializer.new(munchie)
+
+    if road_trip.instance_of?(RoadTrip)
+      munchie = MunchiesFacade.find_munchies(cords, params[:food], road_trip)
+      if munchie.instance_of?(Munchie)
+        render json: MunchieSerializer.new(munchie)
+      else
+        render json: { message: 'unsuccessful', error: 'You must provide a food' },
+               status: :bad_request
+      end
     else
-      render json: { message: 'unsuccessful', error: 'Something went wrong' },
+      render json: { message: 'unsuccessful', error: road_trip[:info][:messages].join },
              status: :bad_request
     end
   end
