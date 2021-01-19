3d1b38c844bf68fbb74a9c19239143e5008b889c
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-18 20:42:49 -0700

    Add guard statements for bad request

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::ForecastController < ApplicationController
   def show
     cords = MapQuestFacade.get_cords(params[:location])
     weather = WeatherFacade.get_weather(cords[:lat], cords[:lng])
-    render json: ForecastSerializer.new(weather)
+    if weather.instance_of?(Forecast)
+      render json: ForecastSerializer.new(weather)
+    else
+      render json: { message: 'unsuccessful', error: weather[:message] },
+             status: :bad_request
+    end
   end
 end
